e0d7f7b97899767690840d32ce67e3e1e463bfb4
Roderick MacLeod <roderickjmacleod@gmail.com>
2020-02-01 16:15:25 -0800

    does a single random walk, 10 steps, works for 1-10 dimension

--- randomwalk.py
+++ randomwalk.py
@@ -4,6 +4,19 @@
 # about the walks (TBD).
 
 import numpy as np
+import random as rand
+
+def take_step(loc, walk, step):
+    x = rand.random()
+    # x is btwn 0 and 1
+    for i in range(2*d):
+        if i*ival <= x < (i+1)*ival:
+            if i in range(d):
+                loc[i] = loc[i] + 1
+            else:
+                loc[i-d] = loc[i-d] - 1
+    walk[step] = loc
+    
 
 while True:
     d = input("Dimension: ")
@@ -17,4 +30,23 @@ while True:
     except ValueError:
         print("Choose an integer between 1 and 10.")
 
-print(d)
+
+steps = 10
+ival = 1/(2*d) # the interval for determining a random direction uning rand.random()
+               # there are 2d possible directions, each one with 1/2d probability.
+loc = np.zeros(d)
+print(loc)
+walk = np.zeros((steps,d))
+current_step = 0
+
+while current_step < steps:
+    take_step(loc, walk, current_step)
+    print(loc)
+    current_step += 1
+
+print("Final location: ")
+print(loc)
+
+
+
+        
